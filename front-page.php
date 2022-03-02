2cd572db4bd6ea7ec0af7a267acc1540f35bf922
Mladen Trickovic <mladen.trickovic5@gmail.com>
2022-03-02 11:36:37 +0100

    default style, hero section structure

--- front-page.php
+++ front-page.php
@@ -2,7 +2,18 @@
 /* Template Name: Homepage Template */
 get_header();
 ?>
+<main id="primary" class="site-main">
 
+    <div class="container hero-wrapper">
+        <div class="hero">
+            <div class="hero-copy">
+                <p class="hero-notification"></p>
+                <h1>Pediatric Integrative Medicine</h1>
+                <p>Building bridges between conventional and complementary medicine</p>
+                <a class="btn" href="#">register</a>
+            </div>
+        </div>
+    </div>
 
-
+</main>
 <?php get_footer(); ?>
